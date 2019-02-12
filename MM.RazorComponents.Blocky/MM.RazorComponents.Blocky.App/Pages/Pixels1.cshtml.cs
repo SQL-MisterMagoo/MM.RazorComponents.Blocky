453159d09de9e22e1075ba74d68a436a13319f2c
Mister Magoo <mister.magoo+githubsql@gmail.com>
2019-02-12 02:15:06 +0000

    Visual tweaks

--- MM.RazorComponents.Blocky/MM.RazorComponents.Blocky.App/Pages/Pixels1.cshtml.cs
+++ MM.RazorComponents.Blocky/MM.RazorComponents.Blocky.App/Pages/Pixels1.cshtml.cs
@@ -7,14 +7,16 @@ using SkiaSharp;
 
 namespace MM.RazorComponents.Blocky.App
 {
-	
+
 	public class SkiaPictureBase : ComponentBase
 	{
-		private const double MAXDIM = 200;
+		public const double MAXDIM = 150;
 		public List<Colors> Pixels;
 		public int PixelSize = 1;
 		public int PictureWidth = 0;
 		public int PictureHeight = 0;
+		public bool IsLoading = true;
+		public int Progress;
 		Task Loader;
 		protected override void OnInit()
 		{
@@ -23,7 +25,7 @@ namespace MM.RazorComponents.Blocky.App
 		async Task LoadImage()
 		{
 			await Task.Delay(0);
-			
+
 			var filepath = @"wwwroot\images\dotnetbot.jpg";
 			var bytes = await Task.FromResult(System.IO.File.ReadAllBytes(filepath));
 			Console.WriteLine($"Got some bytes: {bytes.Length} {bytes.ToString()}");
@@ -31,50 +33,49 @@ namespace MM.RazorComponents.Blocky.App
 			{
 				using (SKBitmap imageInitial = SKBitmap.Decode(stream))
 				{
-					for (int i = 1; i <= 5; i++)
+					Pixels = new List<Colors>();
+					PixelSize = 2;
+					SKBitmap image;
+					if (imageInitial.Width > MAXDIM || imageInitial.Height > MAXDIM)
 					{
-						Pixels = new List<Colors>();
-						int newSize = (int)(MAXDIM * i) / 5;
-						PixelSize = 5 / i;
-						SKBitmap image;
-						if (imageInitial.Width > newSize || imageInitial.Height > newSize)
+						var newWidth = imageInitial.Width;
+						var newHeight = imageInitial.Height;
+						double scale = 1;
+						if (newWidth > newHeight)
 						{
-							var newWidth = imageInitial.Width;
-							var newHeight = imageInitial.Height;
-							double scale = 1;
-							if (newWidth > newHeight)
-							{
-								scale = (newSize / (double)newWidth);
-							}
-							else
-							{
-								scale = (newSize / (double)newHeight);
-							}
-							var newInfo = imageInitial.Info.WithSize((int)(newWidth * scale), (int)(newHeight * scale));
-							image = imageInitial.Resize(newInfo, SKFilterQuality.High);
+							scale = (MAXDIM / (double)newWidth);
 						}
 						else
 						{
-							image = imageInitial;
+							scale = (MAXDIM / (double)newHeight);
 						}
-						PictureWidth = image.Width;
-						PictureHeight = image.Height;
-						Console.WriteLine($"Image is {PictureWidth} x {PictureHeight}");
+						var newInfo = imageInitial.Info.WithSize((int)(newWidth * scale), (int)(newHeight * scale));
+						image = imageInitial.Resize(newInfo, SKFilterQuality.High);
+					}
+					else
+					{
+						image = imageInitial;
+					}
+					PictureWidth = image.Width;
+					PictureHeight = image.Height;
 
-						for (int y = 0; y < image.Height; y++)
+					for (int y = 0; y < image.Height; y++)
+					{
+						for (int x = 0; x < image.Width; x++)
 						{
-							for (int x = 0; x < image.Width; x++)
-							{
-								SKColor sKColor = image.GetPixel(x, y);
-								sKColor.ToHsl(out float h, out float s, out float l);
-								Pixels.Add(new Colors(h, s, l, sKColor.Alpha));
-							}
+							SKColor sKColor = image.GetPixel(x, y);
+							sKColor.ToHsl(out float h, out float s, out float l);
+							Pixels.Add(new Colors(h, s, l, sKColor.Alpha));
 						}
+						Progress++;
 						await Invoke(StateHasChanged);
 						await Task.Delay(1);
 					}
 				}
 			}
+			IsLoading = false;
+			await Invoke(StateHasChanged);
 		}
 	}
 }
+
