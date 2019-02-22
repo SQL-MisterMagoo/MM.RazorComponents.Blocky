6031a22d45a7f2f5cb3d22b4db0e2b35975afbc4
Mister Magoo <mister.magoo+githubsql@gmail.com>
2019-02-22 11:36:22 +0000

    Add CSS Cascade to nav. Fix CSS in Blazor

--- MM.RazorComponents.Blocky/MM.RazorComponents.Blocky.Shared/PixelData.cs
+++ MM.RazorComponents.Blocky/MM.RazorComponents.Blocky.Shared/PixelData.cs
@@ -6,13 +6,20 @@
 		public double C2;
 		public double C3;
 		public double A;
-		public Colors(double c1, double c2, double c3, double a)
+		public ColorType ColorType;
+		public Colors(double c1, double c2, double c3, double a, ColorType colorType = ColorType.RGBA)
 		{
 			C1 = c1;
 			C2 = c2;
 			C3 = c3;
 			A = a;
+			ColorType = colorType;
 		}
+	}
+	public enum ColorType
+	{
+		RGBA,
+		HSLA
 	}
 	public struct Vector2D
 	{
@@ -31,6 +38,10 @@
 		public Colors Colors;
 		public Vector2D Position;
 		public Vector2D Motion;
+		public Vector2D? StartPosition;
+		public Vector2D? ArcPosition;
+		public Vector2D? ScaleRange;
+		public Vector2D? OpacityRange;
 		public double Scale;
 		public bool ShouldDie { get; private set; }
 		public int Size => (int)Scale;
